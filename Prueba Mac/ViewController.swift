eaf02cb915812a1ea79d18c35e2f3e504f9128ac
Erwin <ierwinx@gmail.com>
2021-05-13 17:52:08 -0500

    Initial Commit

---
+++ Prueba Mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Prueba Mac
+//
+//  Created by Erwin on 13/05/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
